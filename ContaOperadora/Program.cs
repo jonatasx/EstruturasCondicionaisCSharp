76d37eb1e8a80410edd5295454ad8371c85a3b8c
jonatasx <jonatas.santospessoa@gmail.com>
2022-10-06 16:19:36 -0300

    Exemplos de Exercicios de Estrutura Condicional em C#

---
+++ ContaOperadora/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+//program that make a math count about a cellphone call minutes charge
+namespace conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100)
+            {
+                conta +=  (minutos - 100) * 2.0;
+            }
+            Console.WriteLine("Valor a pagar : R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MenorEC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//system that show de lower number
+// sistema que mostra o menor numero
+namespace menor
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x, y, z;
+
+        string[] vet = Console.ReadLine().Split(' ');
+
+        x = int.Parse(vet[0]);
+        y= int.Parse(vet[1]);
+        z = int.Parse(vet[2]);
+
+        if(x< y && x < z)
+        {
+            Console.WriteLine("Menor = " + x);
+        }
+        else if (y < z)
+            {
+                Console.WriteLine("Menor = " + y);
+            }
+            else
+            {
+                Console.WriteLine("Menor = " + z);
+            }
+        
+
+        Console.ReadLine();
+    }
+}
+}
